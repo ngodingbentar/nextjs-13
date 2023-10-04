f2aabc4019857601c3311073f0e5e8402dcaa206
ngodingbentar <dewaqintoro@gmail.com>
2023-10-04 17:25:37 +0700

    init

---
+++ app/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+import './header.css'
+function Header () {
+  return (
+    <header className="header p-5 bg-blue-600">
+      <Link href="/" className="px-2 py-1 bg-white text-blue-500">
+        Home
+      </Link>
+
+      <Link href="/todos" className="px-2 py-1 bg-white text-blue-500">
+        Todos
+      </Link>
+    </header>
+  )
+}
+
+export default Header
---
+++ app/todos/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodosList from './TodosList'
+
+function Todos() {
+  return (
+    <>
+      <div>Todos</div>
+      <TodosList />
+    </>
+  )
+}
+
+export default Todos
